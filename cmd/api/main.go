1d82e3009e8ed9a7c0112915efeb4deb622a787a
mmk31585 <mmkabiri31585@gmail.com>
2026-08-21 13:06:44 +0330

    feat(config): add config file + import godotenv package for load envierment + use in main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,12 @@
-package api
+package main
 
-func main(){
-	
+import (
+	"github.com/mmk31585/workout-tracker/internal/config"
+)
+
+func main() {
+	_, err := config.Load()
+	if err != nil {
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mmk31585/workout-tracker
 
 go 1.26.6
+
+require github.com/joho/godotenv v1.5.1
