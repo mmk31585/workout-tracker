4ff672cdf454f6745e33e9bac9c94954c30cf1f2
mmk31585 <mmkabiri31585@gmail.com>
2026-08-21 14:19:25 +0330

    feat: use logger with configs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/mmk31585/workout-tracker/internal/config"
+	"github.com/mmk31585/workout-tracker/internal/logger"
 )
 
 func main() {
-	_, err := config.Load()
+	cfg, err := config.Load()
 	if err != nil {
 		return
 	}
+	logger := logger.New(cfg.App.AppEnv, cfg.Log.LogLevel, cfg.Log.LogFormat)
+	logger.Info("Server initilais complited :)")
 }
