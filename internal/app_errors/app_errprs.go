67bb09e4706081c38376376b3bde9e017340912c
mmk31585 <mmkabiri31585@gmail.com>
2026-08-24 13:53:54 +0330

    feat: change name package + check row err unique

---
+++ internal/app_errors/app_errprs.go
@@ -0,0 +1,23 @@
+package apperrors
+
+import (
+	"errors"
+
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+var (
+	ErrNotFound     = errors.New("resource not found")
+	ErrConflict     = errors.New("resource conflict")
+	ErrInvalidInput = errors.New("invalid input")
+	ErrForeignKey   = errors.New("foreign key violation")
+	ErrInternal     = errors.New("internal error")
+)
+
+func IsUniqueRow(err error) bool {
+	if pgErr, ok := errors.AsType[*pgconn.PgError](err); ok {
+		return pgErr.Code == "23505"
+	}
+
+	return false
+}
