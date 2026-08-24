7626fed43b20b183852a9a6e70128a5a455e0146
mmk31585 <mmkabiri31585@gmail.com>
2026-08-24 13:54:31 +0330

    feat: add db tag for strucr in sqlx

--- internal/workout_plan/model.go
+++ internal/workout_plan/model.go
@@ -3,22 +3,23 @@ package workoutplan
 import "time"
 
 type WorkoutPlan struct {
-	ID          string
-	UserID      string
-	Title       string
-	Description string
-	Status      string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          string    `db:"id"`
+	UserID      string    `db:"user_id"`
+	Title       string    `db:"title"`
+	Description string    `db:"description"`
+	Status      string    `db:"status"`
+	CreatedAt   time.Time `db:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at"`
 }
+
 type WorkoutPlanItem struct {
-	ID            string
-	WorkoutPlanID string
-	ExerciseID    string
-	OrderIndex    int
-	Sets          int
-	Reps          int
-	Weight        float64
-	Unit          string
-	CreatedAt     time.Time
+	ID            string    `db:"id"`
+	WorkoutPlanID string    `db:"workout_plan_id"`
+	ExerciseID    string    `db:"exercise_id"`
+	OrderIndex    int       `db:"order_index"`
+	Sets          int       `db:"sets"`
+	Reps          int       `db:"reps"`
+	Weight        float64   `db:"weight"`
+	Unit          string    `db:"unit"`
+	CreatedAt     time.Time `db:"created_at"`
 }
