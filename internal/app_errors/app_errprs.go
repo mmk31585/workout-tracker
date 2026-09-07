cff84cd07efb2d3f439e2a369ef45eadb96e85cd
mmk31585 <mmkabiri31585@gmail.com>
2026-09-07 07:01:12 +0330

    feat: add errors

--- internal/app_errors/app_errprs.go
+++ internal/app_errors/app_errprs.go
@@ -7,11 +7,13 @@ import (
 )
 
 var (
-	ErrNotFound     = errors.New("resource not found")
-	ErrConflict     = errors.New("resource conflict")
-	ErrInvalidInput = errors.New("invalid input")
-	ErrForeignKey   = errors.New("foreign key violation")
-	ErrInternal     = errors.New("internal error")
+	ErrNotFound           = errors.New("resource not found")
+	ErrConflict           = errors.New("resource conflict")
+	ErrInvalidInput       = errors.New("invalid input")
+	ErrForeignKey         = errors.New("foreign key violation")
+	ErrInternal           = errors.New("internal error")
+	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrUnauthorized       = errors.New("unauthorized")
 )
 
 func IsUniqueRow(err error) bool {
